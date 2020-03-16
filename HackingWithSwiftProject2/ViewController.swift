2a90135743ad8111b4a0e6e816c201a62c3f00d4
Chen Sihan <schen@lcs.on.ca>
2020-03-16 02:59:37 -0400

    add comment + add tags + add ui AlertController + finish buttontapped action

--- HackingWithSwiftProject2/ViewController.swift
+++ HackingWithSwiftProject2/ViewController.swift
@@ -14,14 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
-    
+    var score = 0
+    var correctAnswer = 0
+    var countries = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Initial Values
-        var countries = [String]()
-        var correctAnswer = 0
-        var score = 0
+        
         countries += ["estonia", "france", "germany", "ireland",
                       "italy", "monaco", "nigeria", "poland", "russia", "spain",
                       "uk", "us"]
@@ -36,21 +37,44 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
-        //
+        // Function for asking questions
         
-        func askQuestion() {
+        func askQuestion(action: UIAlertAction! = nil) {
             countries.shuffle()
+            // load the image
             button1.setImage(UIImage(named: countries[0]), for: .normal)
             button2.setImage(UIImage(named: countries[1]), for: .normal)
             button3.setImage(UIImage(named: countries[2]), for: .normal)
             
             correctAnswer = Int.random(in: 0...2)
-            
+            // Uppercase
             title = countries[correctAnswer].uppercased()
+            
+            
+            let ac = UIAlertController(title: title, message: "Your score is \(score)" , preferredStyle: .alert)
+            present(ac, animated: true)
         }
         
+        // When it's not specified, parameter has a value of nil
         askQuestion()
         
+    }
+ 
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // Correction
+        var title: String
+        if sender.tag == correctAnswer {
+           title = "Correct"
+           score += 1
+        } else {
+           title = "Wrong"
+           score -= 1
+        }
+        
+        
+     
     }
     
     
