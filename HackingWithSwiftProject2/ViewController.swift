db4c3fcd4b1fab63d007ef19ead25b6dc894215a
Chen Sihan <schen@lcs.on.ca>
2020-03-16 02:00:01 -0400

    add border + set border color + shuffle the countries being displayed + let title be uppercased

--- HackingWithSwiftProject2/ViewController.swift
+++ HackingWithSwiftProject2/ViewController.swift
@@ -20,23 +20,40 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // Initial Values
         var countries = [String]()
+        var correctAnswer = 0
         var score = 0
         countries += ["estonia", "france", "germany", "ireland",
-        "italy", "monaco", "nigeria", "poland", "russia", "spain",
-        "uk", "us"]
+                      "italy", "monaco", "nigeria", "poland", "russia", "spain",
+                      "uk", "us"]
+        
+        // Give borders to the flags to prevent the confusion of white stripes
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // Change border color to lightgrey(notice the change from UIColor to CgColor)
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
         
         //
         
         func askQuestion() {
-           button1.setImage(UIImage(named: countries[0]), for: .normal)
-           button2.setImage(UIImage(named: countries[1]), for: .normal)
-           button3.setImage(UIImage(named: countries[2]), for: .normal)
+            countries.shuffle()
+            button1.setImage(UIImage(named: countries[0]), for: .normal)
+            button2.setImage(UIImage(named: countries[1]), for: .normal)
+            button3.setImage(UIImage(named: countries[2]), for: .normal)
+            
+            correctAnswer = Int.random(in: 0...2)
+            
+            title = countries[correctAnswer].uppercased()
         }
+        
         askQuestion()
         
     }
     
     
     
-
+    
 }
